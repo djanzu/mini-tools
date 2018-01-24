a417466a37e770442c657aea9738d9c2eef6c2bb
Anzu Takahashi <anzu.takahashi@sizebook.co.jp>
2018-01-24 11:48:03 +0900

    -y忘れてた

--- install-docker.sh
+++ install-docker.sh
@@ -3,7 +3,7 @@ set -eu
 
 echo "start."
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -14,7 +14,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 sudo docker -v
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
