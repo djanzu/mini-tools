a94cc11585defd969906e9d20f6a716a21bbf674
Anzu Takahashi <anzu.takahashi@sizebook.co.jp>
2018-02-20 16:27:08 +0900

    sudoしてからじゃないとダメだった…

--- makeswap.sh
+++ makeswap.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 set -eu
 
-sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon
-sudo echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
+ dd if=/dev/zero of=/swapfile bs=1M count=1024
+ chmod 600 /swapfile
+ mkswap /swapfile
+ swapon
+#  echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 df -h
-
-
-
--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 # how to use
+
+## install docker
 - ``curl https://raw.githubusercontent.com/anzu-so-happy/mini-tools/master/install-docker.sh | sh`` でdocker, docker-composeのインストール
 - dockerコマンドはsudoでやるのでグループの追加などはしません
 
-
+## make swapfile
+- 一旦sudoしてちょ…
+```
+sudo su
+curl https://raw.githubusercontent.com/anzu-so-happy/mini-tools/master/makeswap.sh | sh
+```
